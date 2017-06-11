f7c712f8635266c37da91fa977639e28ded76e76
DESKTOP-O0ILD7S\lenovo <nhxyy.good@163.com>
2017-06-11 10:02:04 +0800

    6.11

---
+++ tetris1/GameLay.cpp
@@ -0,0 +1,85 @@
+#include "GameLay.h"
+#include "GameScene.h"
+
+USING_NS_CC;
+int pattern;
+Scene* GameLay::createScene()
+{
+	// 'scene' is an autorelease object
+	auto scene = Scene::create();
+
+	// 'layer' is an autorelease object
+	auto layer = GameLay::create();
+
+	// add layer as a child to scene
+	scene->addChild(layer);
+
+	// return the scene
+	return scene;
+}
+
+// on "init" you need to initialize your instance
+bool GameLay::init()
+{
+	//////////////////////////////
+	// 1. super init first
+	if (!Layer::init())
+	{
+		return false;
+	}
+
+	Size visibleSize = Director::getInstance()->getVisibleSize();
+	Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+	//Ä£Ê½Ñ¡Ôñ
+	auto easyItem = MenuItemImage::create(
+		"easy.png",
+		"easy.png",
+		CC_CALLBACK_1(GameLay::easyStartCallback, this));
+	easyItem->setPosition(Vec2(240, 400));
+	easyItem->setScale(1.2f);
+
+	auto medItem = MenuItemImage::create(
+		"challenge.png",
+		"challenge.png",
+		CC_CALLBACK_1(GameLay::medicalStartCallback, this));
+	medItem->setPosition(Vec2(240, 250));
+	medItem->setScale(1.2f);
+
+	auto hardItem = MenuItemImage::create(
+		"hard.png",
+		"hard.png",
+		CC_CALLBACK_1(GameLay::hardStartCallback, this));
+	hardItem->setPosition(Vec2(240, 100));
+	hardItem->setScale(1.2f);
+
+	auto menu = Menu::create(easyItem, medItem, hardItem, NULL);
+	menu->setPosition(Point::ZERO);
+	this->addChild(menu, 1);
+
+
+	return true;
+}
+
+void GameLay::easyStartCallback(cocos2d::Ref* pSender)
+{
+	pattern = 1;
+	Director::getInstance()->replaceScene(TransitionFade::create(1.2f, GameScene::createScene()));
+	SimpleAudioEngine::getInstance()->stopBackgroundMusic();
+	SimpleAudioEngine::getInstance()->playBackgroundMusic("music2.mp3", true);
+}
+
+void GameLay::medicalStartCallback(cocos2d::Ref* pSender)
+{
+	pattern = 2;
+	Director::getInstance()->replaceScene(TransitionFade::create(1.2f, GameScene::createScene()));
+	SimpleAudioEngine::getInstance()->stopBackgroundMusic();
+	SimpleAudioEngine::getInstance()->playBackgroundMusic("music2.mp3", true);
+}
+void GameLay::hardStartCallback(cocos2d::Ref* pSender)
+{
+	pattern = 3;
+	Director::getInstance()->replaceScene(TransitionFade::create(1.2f, GameScene::createScene()));
+	SimpleAudioEngine::getInstance()->stopBackgroundMusic();
+	SimpleAudioEngine::getInstance()->playBackgroundMusic("music2.mp3", true);
+}
---
+++ tetris1/GameLay.h
@@ -0,0 +1,20 @@
+#ifndef _GAMELAY_H 
+#define _GAMELAY_H
+
+#include "cocos2d.h"
+class GameLay : public cocos2d::Layer
+{
+public:
+	static cocos2d::Scene* createScene();
+	virtual bool init();
+
+	// a selector callback
+	void easyStartCallback(cocos2d::Ref* pSender);
+	void medicalStartCallback(cocos2d::Ref* pSender);
+	void hardStartCallback(cocos2d::Ref* pSender);
+	// implement the "static create()" method manually
+	CREATE_FUNC(GameLay);
+
+};
+
+#endif
